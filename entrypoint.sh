3127fff6a73b9d61bb0b5020f5c38adbe9192eb3
Yahaya Azeez <azconcept2016@gmail.com>
2024-02-02 08:40:23 +0000

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Goto /Api/V8/OAuth2 and run
+
+cd /srv/app/Api/V8/OAuth2
+
+openssl genrsa -out private.key 2048
+
+openssl rsa -in private.key -pubout -out public.key
+
+chmod 600 private.key public.key
+
+chown www-data:www-data p*.key
+
+# for lemp stack
+set -e
+
+php-fpm -D
+nginx -g 'daemon off;'
